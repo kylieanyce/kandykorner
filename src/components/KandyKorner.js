fdd6acc0f51c36f670ce36303fb95902c64e8f58
kylieanyce <kylieanyce@gmail.com>
2021-02-24 21:47:38 -0600

    created kandykorner component

---
+++ src/components/KandyKorner.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./KandyKorner.css"
+
+export const KandyKorner = () => (
+    <>
+        <h2>Nashville Kennels</h2>
+        <small>Loving care when you're not there.</small>
+        <address>
+            <div>Visit Us at the Nashville North Location</div>
+            <div>500 Puppy Way</div>
+        </address>
+    </>
+)
