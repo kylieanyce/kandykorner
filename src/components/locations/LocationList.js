e6ff63af11b07f04c614484e9345581fda180fc4
kylieanyce <kylieanyce@gmail.com>
2021-03-02 10:06:40 -0600

    added locationProvider

---
+++ src/components/locations/LocationList.js
@@ -0,0 +1,6 @@
+import React, { useContext, useEffect } from "react"
+import { useHistory } from "react-router-dom"
+import { LocationContext } from "./LocationProvider"
+import { LocationCard } from "./Location"
+import "./Location.css"
+
