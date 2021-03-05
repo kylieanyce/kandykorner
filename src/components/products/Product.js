46f33354f1fc5c1871c5890ddd657b94311bdf96
kylieanyce <kylieanyce@gmail.com>
2021-03-05 16:12:56 -0600

    added product types to product list on DOM

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -5,5 +5,6 @@ export const ProductCard = ({ product }) => (
     <section className="product">
         <h3 className="product__name">{product.name}</h3>
         <p className="product__">Price: ${product.price}</p>
+        <p className="product__">Type: {product.productType?.productType}</p>
     </section>
 )
--- src/components/products/ProductProvider.js
+++ src/components/products/ProductProvider.js
@@ -5,7 +5,7 @@ export const ProductContext = createContext()
 export const ProductProvider = (props) => {
     const [products, setProducts] = useState([])
     const getProducts = () => {
-        return fetch("http://localhost:8088/products/?_expand=productTypes")
+        return fetch("http://localhost:8088/products/?_expand=productType")
         .then(res => res.json())
         .then(setProducts)
     }
