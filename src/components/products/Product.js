f96cea70d4336893e6aa3aa73e574260ce47caa9
kylieanyce <kylieanyce@gmail.com>
2021-03-05 15:28:36 -0600

    working on displaying product types with each product

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -4,6 +4,6 @@ import "./Product.css"
 export const ProductCard = ({ product }) => (
     <section className="product">
         <h3 className="product__name">{product.name}</h3>
-        <p className="product__type">Type: {product.type}</p>
+        <p className="product__">Price: ${product.price}</p>
     </section>
 )
--- src/components/products/ProductProvider.js
+++ src/components/products/ProductProvider.js
@@ -5,14 +5,20 @@ export const ProductContext = createContext()
 export const ProductProvider = (props) => {
     const [products, setProducts] = useState([])
     const getProducts = () => {
-        return fetch("http://localhost:8088/products")
+        return fetch("http://localhost:8088/products/?_expand=productTypes")
         .then(res => res.json())
         .then(setProducts)
     }
 
+    const getProductById = (id) => {
+        return fetch(`http://localhost:8088/products/${id}?_embed=productTypes`)
+        .then(res => res.json())
+    }
+
     return (
         <ProductContext.Provider value={{
-            products, getProducts}}>
+            products, getProducts, getProductById
+            }}>
                 {props.children}
             </ProductContext.Provider>
     )
